df337e3b2d8fdea8b7ba617800de78d9fdf43fcf
zionlio <470522904@qq.com>
2025-09-27 22:04:26 +0800

    修改默认api地址为自建api地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,8 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+   // "https://admin.rustdesk.com".to_owned()
+    "http://nas.zionlio.com:21111/".to_owned()
 }
 
 #[inline]
